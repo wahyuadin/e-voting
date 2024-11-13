554df1309b47e2936264bc9aa62e3ced8f2447d2
Wahyuadin.zip <mohammadwahyuadinugroho@gmail.com>
2024-11-13 21:01:25 +0700

    update dashboard

--- resources/views/admin/management-user.blade.php
+++ resources/views/admin/management-user.blade.php
@@ -129,7 +129,7 @@
                                     <div class="modal-body">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h1 class="modal-title fs-5" id="exampleModalLabel">{{ $title }}</h1>
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel"></h1>
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
                                             <div class="modal-body">
